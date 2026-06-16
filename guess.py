70c1a31c37fddc83f293c1ea45cede760ad1084b
Jason Nsiamfumu <jasonnsiamfumu1@gmail.com>
2026-06-16 15:58:57 +0100

    Ajout des niveaux

--- guess.py
+++ guess.py
@@ -1,8 +1,20 @@
 import random
 
-print("WELCOME!".center(20, "="))
-num_à_deviner = random.randint(1, 100)
-tentatives = 0
+print("WELCOME!".center(50, "="))
 
 while True:
+    niveau = input("Veuillez saisir le niveau de difficulté (facile, moyen, difficile) : ").lower()
 
+    if niveau == 'facile':
+        nombre_max = 10
+        essai = 4
+
+    elif niveau == 'moyen':
+        nombre_max = 20
+        essai = 6
+    elif niveau == 'difficile':
+        nombre_max = 50
+        essai = 8
+    else:
+        print("Niveau de difficulté invalide. Veuillez réessayer.")
+        continue   
