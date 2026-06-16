b8fc995275edec572b2208ea397e676659c3a78a
Jason Nsiamfumu <jasonnsiamfumu1@gmail.com>
2026-06-16 15:03:04 +0100

    Vérification si la venitte est supérieure à celle qui doit être trouvée

--- guess.py
+++ guess.py
@@ -7,3 +7,5 @@ while valeur != machine:
     valeur = int(input("Entrez un nombre entre 1 et 100 : "))
     if valeur < machine:
         print("Trop petit !")
+    elif valeur > machine:
+        print("Trop grand !")
