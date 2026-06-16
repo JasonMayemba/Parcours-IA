2ec34b6f22df47c122c6e1de8ba1cd0967783d19
Jason Nsiamfumu <jasonnsiamfumu1@gmail.com>
2026-06-16 16:25:44 +0100

    Possibilité de vouloir recommencer le jeu

--- guess.py
+++ guess.py
@@ -22,5 +22,26 @@ while True:
     guessing_number = random.randint(1, nombre_max)
 
     print(f"Vous avez choisi le niveau {niveau}.\nVous devez deviner un nombre entre 1 et {nombre_max}.\nVous avez {essai} essais pour deviner le nombre.")
+    réponse = False
+
+    for _ in range(1, essai + 1):
+        try:
+            user_guess = int(input(f"{_} / {essai} essai(s) que vous faites : "))
+            if user_guess < guessing_number:
+                print("Trop bas ! Essayez encore.")
+
+            elif user_guess > guessing_number:
+                print("Trop haut ! Essayez encore.")
+
+            else:
+                print(f"Félicitations ! Vous avez deviné le nombre {guessing_number} en {_} essai(s) !")
+                break
     
+    if not réponse:
+        print(f"Le nombre correct était {guessing_number}.")
+
+    recommencer = input("Voulez-vous recommencer le jeu ? (oui/non) : ").lower()
+    if recommencer != 'oui':
+        print("Merci d'avoir joué ! À bientôt !")
+        break
 
