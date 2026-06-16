c0dbbce4cf2efb844b6cea42d65b623d6eec87bd
Jason Nsiamfumu <jasonnsiamfumu1@gmail.com>
2026-06-16 15:15:40 +0100

    Affichage du nombres d'essai

--- guess.py
+++ guess.py
@@ -6,7 +6,10 @@ machine = random.randint(1, 100)
 
 while valeur != machine:
     valeur = int(input("Entrez un nombre entre 1 et 100 : "))
+    tentative = tentative + 1 
     if valeur < machine:
         print("Trop petit !")
     elif valeur > machine:
         print("Trop grand !")
+
+print("Bravo ! Vous avez trouvé le nombre en", tentative, "tentatives.")
