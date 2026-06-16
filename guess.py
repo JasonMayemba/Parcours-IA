626022812dafd60c0f5891517354711fb4e90784
Jason Nsiamfumu <jasonnsiamfumu1@gmail.com>
2026-06-16 15:01:39 +0100

    Utilisation de while/instruction

--- guess.py
+++ guess.py
@@ -1,13 +1,9 @@
 import random
 
-valeur = int(input("Entrez un nombre entre 1 et 100: "))
+valeur = 0
 machine = random.randint(1, 100)
 
-if valeur < machine :
-    print("Le nombre est petit !")
-
-elif valeur > machine : 
-    print("Le nombre est grand !")
-
-else:
-    print("Bingo ! Vous avez deviné le nombre !")
+while valeur != machine:
+    valeur = int(input("Entrez un nombre entre 1 et 100 : "))
+    if valeur < machine:
+        print("Trop petit !")
