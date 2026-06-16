b86f9ebbb2d465390977fdae55c21d2f43d243c0
Jason Nsiamfumu <jasonnsiamfumu1@gmail.com>
2026-06-16 14:10:45 +0100

    Ajout des conditions

--- guess.py
+++ guess.py
@@ -2,3 +2,9 @@ import random
 
 valeur = int(input("Entrez un nombre entre 1 et 100: "))
 machine = random.randint(1, 100)
+
+if valeur < machine :
+    print("Le nombre est petit !")
+
+elif valeur > machine : 
+    print("Le nombre est grand !")
